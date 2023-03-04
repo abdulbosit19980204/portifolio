652f1d2d1ae4077a3bddc7d0936fc5c2321db104
Abdulbosit <71276545+abdulbosit19980204@users.noreply.github.com>
2023-03-04 21:27:08 +0000

    s

--- assets/js/disableInsMode.js
+++ assets/js/disableInsMode.js
@@ -1,15 +1,15 @@
-// document.addEventListener("contextmenu",function(e){
-//     e.preventDefault();
-// });
-// document.onkeydown= function(e){
-//     if(event.keyCode==123){
-//         return false;
-//     }
-//     if(e.ctrlKey && e.shiftKey && e.keyCode=="I".charCodeAt(0)){return false;}
-//     if(e.ctrlKey && e.shiftKey && e.keyCode=="C".charCodeAt(0)){return false;}
-//     if(e.ctrlKey && e.shiftKey && e.keyCode=="J".charCodeAt(0)){return false;}
-//     if(e.ctrlKey && e.keyCode=="U".charCodeAt(0)){return false;}
-    
-// };
+document.addEventListener("contextmenu",function(e){
+    e.preventDefault();
+    e.requestFullsecreen();
+});
+document.onkeydown= function(e){
+    if(event.keyCode==123){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="I".charCodeAt(0)){return false;}
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="C".charCodeAt(0)){return false;}
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="J".charCodeAt(0)){return false;}
+    if(e.ctrlKey && e.keyCode=="U".charCodeAt(0)){return false;}
+    e.requestFullsecreen();
+};
 
-oncontextmenu.requestFullsecreen();
